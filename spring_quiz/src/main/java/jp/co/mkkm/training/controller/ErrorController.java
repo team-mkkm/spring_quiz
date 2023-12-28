0eccadb8df7905127dcba5f4f51d75ede3851ba3
y-matsuda <y-matsuda@3sss.co.jp>
2023-12-28 15:07:37 +0900

    /errorは予約済みでした

--- spring_quiz/src/main/java/jp/co/mkkm/training/controller/ErrorController.java
+++ spring_quiz/src/main/java/jp/co/mkkm/training/controller/ErrorController.java
@@ -1,10 +1,12 @@
 package jp.co.mkkm.training.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class ErrorController {
 	
-	@RequestMapping(path = "/error")
+	@RequestMapping(path = "/bug")
 	public String error() {
 		return "error/error404";
 	}	
