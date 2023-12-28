3d4415a2c69848b8e04070c95ded460aaf2d4ed5
y-matsuda <y-matsuda@3sss.co.jp>
2023-12-28 14:08:55 +0900

    dailyappに遷移します

--- spring_quiz/src/main/java/jp/co/mkkm/training/controller/DailyController.java
+++ spring_quiz/src/main/java/jp/co/mkkm/training/controller/DailyController.java
@@ -1,5 +1,15 @@
 package jp.co.mkkm.training.controller;
 
-public class DailyController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DailyController {	
+	
+	@RequestMapping(path = "/daily")
+	public String quiz() {
+		return "daily/dailyapp";
+	}	
+	
 
 }
