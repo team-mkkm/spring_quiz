92d2f1309635caffb3129b47bd2fe0227bbcbd98
m-kondou <masakikondou0419@gmail.com>
2023-12-28 15:16:59 +0900

    ageage

--- spring_quiz/src/main/java/jp/co/mkkm/training/controller/ErrorController.java
+++ spring_quiz/src/main/java/jp/co/mkkm/training/controller/ErrorController.java
@@ -1,12 +1,14 @@
 package jp.co.mkkm.training.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class ErrorController {
-	
-	@RequestMapping(path = "/error")
+
+	@RequestMapping(path = "/")
 	public String error() {
 		return "error/error404";
-	}	
+	}
 
 }
